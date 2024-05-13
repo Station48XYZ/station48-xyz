851f8f8a2e6a79e3fbadeec26d2308cac8b37a2d
Adam Matthiesen <amatthiesen@outlook.com>
2024-05-13 07:40:48 -0700

    add image domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: "https://studio48.xyz",
+    image: {
+        domains: ["status.mclive.eu"]
+    }
 });
