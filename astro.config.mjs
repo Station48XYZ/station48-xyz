5fef4bec0c13f724d17d7a16ac4aa1174234c7a9
Adam Matthiesen <amatthiesen@outlook.com>
2024-05-26 22:58:41 -0700

    New website!

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,23 @@
 import { defineConfig } from 'astro/config';
+import minecraftStyles from 'astrocraft/unocss';
+import mcServerStatus from '@matthiesenxyz/astro-mcserverstatus';
+import mdx from "@astrojs/mdx";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://studio48.xyz",
-    image: {
-        domains: ["status.mclive.eu"]
-    }
+  site: "https://studio48.xyz",
+  output: "server",
+  adapter: node({ mode: "standalone" }),
+  integrations: [
+    mdx(), 
+    minecraftStyles(), 
+    mcServerStatus({
+        serverAddress: "play.station48.xyz",
+        javaOptions: {
+            query: true,
+        },
+        verbose: true,
+    })
+  ]
 });
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,4 @@
+/// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+/// <reference types="../.astro/astro-mcserverstatus-components.d.ts" />
+/// <reference types="../.astro/astro-mcserverstatus-helpers.d.ts" />
