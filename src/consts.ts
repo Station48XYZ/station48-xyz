719ccaa4b138c523c0fb4788adfa246e3e524a68
Adam Matthiesen <amatthiesen@outlook.com>
2025-05-31 09:32:23 -0700

    Refactor navigation and update Modrinth link to use constants

---
+++ src/consts.ts
@@ -0,0 +1,24 @@
+export const MODRINTN_PACK_URL = 'https://modrinth.com/modpack/jonathans-cobblemon-pack'
+
+export const NAV = [
+    {
+        text: 'Home',
+        href: '/',
+    },
+    {
+        text: 'Get Started',
+        href: '/get-started',
+    },
+    {
+        text: 'Web Shop (coming soon)',
+        href: '/',
+    },
+    {
+        text: 'Interactive Live Map',
+        href: 'https://map.station48.xyz'
+    },
+    {
+        text: 'Discord',
+        href: 'https://chat.studiocms.dev',
+    }
+]
