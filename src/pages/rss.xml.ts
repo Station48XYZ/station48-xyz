8816c89d8c3a04bd4f262f613ceb6c652761f2ee
Adam Matthiesen <amatthiesen@outlook.com>
2025-06-20 02:01:41 -0700

    Add news collection and RSS feed generation; update 404 and get-started pages

---
+++ src/pages/rss.xml.ts
@@ -0,0 +1,23 @@
+import type { APIContext, APIRoute } from "astro";
+import rss from '@astrojs/rss';
+import { getCollection } from "astro:content";
+
+export const GET: APIRoute = async (context: APIContext) => {
+    // Import the blog collection from the content collections
+    const news = await getCollection('news');
+
+    // Generate the RSS feed using the imported blog collection
+    return rss({
+        title: 'Station48 News',
+        description: 'A all Inclusive and Welcoming Community Minecraft Server for all Players, Devs and non-devs alike! We are a community of developers (Mostly Astro Devs), builders, and players who love to relax, create, and play Minecraft together.',
+        site: context.site,
+        // Array of `<item>`s in output xml
+        // See "Generating items" section for examples using content collections and glob imports
+        items: news.map((post) => ({
+            title: post.data.title,
+            description: post.data.description,
+            pubDate: post.data.date,
+            link: `/news/${post.slug}`
+        })),
+    });
+}
