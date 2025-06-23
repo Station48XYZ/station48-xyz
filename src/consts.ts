80d1ed52cb2cd7786cc0d13809fcd3e80ecc8b83
Adam Matthiesen <amatthiesen@outlook.com>
2025-06-23 14:12:46 -0700

    refactor: Remove unnecessary whitespace and comments for cleaner code

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,5 @@
 /*
-
     External Links
-
 */
 
 export const MODRINTN_PACK_URL = 'https://modrinth.com/modpack/station48'
@@ -9,27 +7,21 @@ export const STATION48_LAUNCHER_DOWNLOAD_URL = 'https://github.com/Station48XYZ/
 export const KINETIC_HOSTING_REFERRAL_URL = 'https://billing.kinetichosting.net/aff.php?aff=1097';
 
 /*
-
     Station48 Links
-
 */
 
 export const DISCORD_URL = 'https://discord.gg/Hq5j3WhX9b';
 export const MAP_URL = "https://map.station48.xyz/#world:101:0:219:602:0:0:0:1:flat"
 
 /*
-
     MC Server Info
-
 */
 
 export const STATION48_SERVER_IP = 'play.station48.xyz';
 export const STATION48_SERVER_PORT = 25565;
 
 /*
-
     Navigation Bar Settings
-
 */
 
 export const NAV = [
